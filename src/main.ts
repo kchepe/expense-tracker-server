9a2c6d259a436571cf585dbae0251a9a3fb3ec00
Bryan Libod-Libod <bryan@invedent.com>
2023-09-17 18:44:13 +0800

    comment out certficate

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as fs from 'fs';
 
-const httpsOptions = {
-  key: fs.readFileSync('./secrets/private.key'),
-  cert: fs.readFileSync('./secrets/certificate.crt'),
-};
+// const httpsOptions = {
+//   key: fs.readFileSync('./secrets/private.key'),
+//   cert: fs.readFileSync('./secrets/certificate.crt'),
+// };
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { httpsOptions });
